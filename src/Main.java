a4c9e370585d1f1217e526a0798a5b5269dcca6b
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-23 13:44:55 +0300

    Домашнее задание 9 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
         task4();
-        task5();
-        task6();
-        task7();
-        task8();}
+    }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -18,46 +14,48 @@ public class Main {
         }
         return arr;
     }
+
     private static void task1() {
         System.out.println("задача 1");
-        int[ ] arr = generateRandomArray();
-        int sum = 0;
-        for (int j : arr) {
-            sum += j;
+        int[] arr = generateRandomArray();
+        int total = 0;
+        for (int monthPay : arr) {
+            total += monthPay;
         }
-        System.out.println("Сумма трат за месяц составила "+sum+" рублей");
-
-
+        System.out.println("Сумма трат за месяц составила " + total + " рублей");
     }
+
     private static void task2() {
         System.out.println("задача 2");
-        int[ ] arr = generateRandomArray();
+        int[] arr = generateRandomArray();
         int maxPay = -1;
-        for (int j : arr) {
-            int current = j;
-            if (current > maxPay) {
-                maxPay = current;
-            }
+        int minPay = 200001;
+        for (int monthPay : arr) {
+            int current = monthPay;
+            if (current > maxPay) maxPay = current;
+            if (current<minPay) minPay = current;
         }
-        int minPay= Arrays.stream(arr).filter(i -> i <= 200_001).min().orElse(200_001);
-        System.out.println("Минимальная сумма трат за день составила "+minPay+" рублей. Максимальная сумма трат за день составила "+maxPay+" рублей");
+        System.out.println("Минимальная сумма трат за день составила " + minPay + " рублей. Максимальная сумма трат за день составила " + maxPay + " рублей");
     }
+
     private static void task3() {
         System.out.println("задача 3");
-
+        int[] arr = generateRandomArray();
+        int totalPay = 0;
+        for (int j : arr) {
+            totalPay += j;
+        }
+        double length = arr.length;
+        double averageTotal = (double) totalPay / length;
+        System.out.printf("Средняя сумма трат за месяц составила %.2f рублей", averageTotal);
     }
+
     private static void task4() {
-        System.out.println("задача 4");}
-
-    private static void task5() {
-        System.out.println("задача 5");}
-
-    private static void task6() {
-        System.out.println("задача 6");}
-
-    private static void task7() {
-        System.out.println("задача 7");}
-
-    private static void task8() {
-        System.out.println("задача 8");}
+        System.out.println();
+        System.out.println("задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+    }
 }
